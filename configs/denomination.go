11502ba4f0ba302231f08baea74e9d472d4b2e23
houyi <zhangyueru@aliyun.com>
2018-12-13 10:54:02 +0800

    Aidoc 面额的乘数。

---
+++ configs/denomination.go
@@ -0,0 +1,17 @@
+package configs
+
+//这些是 Aidoc 面额的乘数。
+//示例：要获取“LiShizhen”中金额的 Dose 值，请使用
+//
+// new（big.Int）.Mul（value，big.NewInt（params.LiShizhen））
+const (
+	Dose           = 1
+	BianQue        = 1e3
+	HuaTuo         = 1e6
+	ZhangZhongjing = 1e9
+	HuangFumi      = 1e12
+	Songci         = 1e15
+	Aidoc          = 1e18
+	SunSimiao      = 1e21
+	LiShizhen      = 1e42
+)
