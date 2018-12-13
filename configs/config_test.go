1933d4f806a42ff88e9dc02bb626e5908e55e001
houyi <zhangyueru@aliyun.com>
2018-12-13 10:52:49 +0800

    config test

---
+++ configs/config_test.go
@@ -0,0 +1,65 @@
+package configs
+
+import (
+	"math/big"
+	"reflect"
+	"testing"
+)
+
+func TestCheckCompatible(t *testing.T) {
+	type test struct {
+		stored, new *ChainConfig
+		head        uint64
+		wantErr     *ConfigCompatError
+	}
+	tests := []test{
+		{stored: AllAidochashProtocolChanges, new: AllAidochashProtocolChanges, head: 0, wantErr: nil},
+		{stored: AllAidochashProtocolChanges, new: AllAidochashProtocolChanges, head: 100, wantErr: nil},
+		{
+			stored:  &ChainConfig{EIP150Block: big.NewInt(10)},
+			new:     &ChainConfig{EIP150Block: big.NewInt(20)},
+			head:    9,
+			wantErr: nil,
+		},
+		{
+			stored: AllAidochashProtocolChanges,
+			new:    &ChainConfig{HomesteadBlock: nil},
+			head:   3,
+			wantErr: &ConfigCompatError{
+				What:         "Homestead 叉块",
+				StoredConfig: big.NewInt(0),
+				NewConfig:    nil,
+				RewindTo:     0,
+			},
+		},
+		{
+			stored: AllAidochashProtocolChanges,
+			new:    &ChainConfig{HomesteadBlock: big.NewInt(1)},
+			head:   3,
+			wantErr: &ConfigCompatError{
+				What:         "Homestead 叉块",
+				StoredConfig: big.NewInt(0),
+				NewConfig:    big.NewInt(1),
+				RewindTo:     0,
+			},
+		},
+		{
+			stored: &ChainConfig{HomesteadBlock: big.NewInt(30), EIP150Block: big.NewInt(10)},
+			new:    &ChainConfig{HomesteadBlock: big.NewInt(25), EIP150Block: big.NewInt(20)},
+			head:   25,
+			wantErr: &ConfigCompatError{
+				What:         "EIP150前叉块",
+				StoredConfig: big.NewInt(10),
+				NewConfig:    big.NewInt(20),
+				RewindTo:     9,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		err := test.stored.CheckCompatible(test.new, test.head)
+		if !reflect.DeepEqual(err, test.wantErr) {
+			t.Errorf("错误不匹配：\n 已存储：%v \n新：%v \n头：%v \n错误：%v \n 需要：%v", test.stored, test.new, test.head, err, test.wantErr)
+		}
+	}
+}
